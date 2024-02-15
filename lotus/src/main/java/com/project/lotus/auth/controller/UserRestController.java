9ab678acb4a356a48c28af11ab7b1f5f0c607115
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-02-15 12:37:15 +0900

    Delete lotus directory

--- lotus/src/main/java/com/project/lotus/auth/controller/UserRestController.java
+++
@@ -1,9 +0,0 @@
-package com.project.lotus.auth.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/user")
-public class UserRestController {
-}
--- lotus/src/main/java/com/project/lotus/common/config/security/Role.java
+++
@@ -1,6 +0,0 @@
-package com.project.lotus.common.config.security;
-
-public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN
-}
