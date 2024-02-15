cd35d07a7c6bb505a56e492eee7ef44d812a1fe1
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-02-15 12:39:00 +0900

    Delete 권지현 lotus directory

--- 권지현 lotus/lotus/src/main/java/com/project/lotus/admin/repository/QnaReplyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.lotus.admin.repository;
-
-import com.project.lotus.admin.entity.QnaReply;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QnaReplyRepository extends JpaRepository<QnaReply,Long> {
-
-}
